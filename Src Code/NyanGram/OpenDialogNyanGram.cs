2ae0626932302259bb32abfd62b4a7d1fd2a09b0
RyuzakiEverywhere aka LLawlietEverywhere <119974711+RyuzakiEverywhere@users.noreply.github.com>
2022-12-25 10:12:48 +0300

    Add files via upload

---
+++ Src Code/NyanGram/OpenDialogNyanGram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace NyanGram
+{
+
+    public class DialogFNyanGram
+    {
+
+        [STAThread]
+        public void OpenDialogAndInject()
+        {
+            Mem mem = new Mem();
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "DLL File | *.dll"; //Filter Name :D
+            file.Title = "Please Select DLL File";
+            file.FileName = String.Empty; //Empty File :D
+            DialogResult result = DialogResult.OK;
+            if(file.ShowDialog() == result)
+            {
+                if (mem.OpenProcess("RustClient"))
+                {
+                    if (mem.InjectDll(file.FileName))
+                    {
+                        Console.WriteLine($"[NyanGram] Injecting Is Successfulled... \nFileName: {file.FileName}");
+                        Thread.Sleep(5000);
+                        Environment.Exit(345);
+                    }
+                    else
+                    {
+                        Console.Write("[NyanGram] Failed To Injecting DLL...");
+                        Thread.Sleep(4500);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Src Code/NyanGram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NyanGram
+{
+    internal  class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            DialogFNyanGram gram = new DialogFNyanGram();
+            Console.WriteLine("[NyanGram] Waiting For Injecting...");
+            gram.OpenDialogAndInject();
+        }
+    }
+}
